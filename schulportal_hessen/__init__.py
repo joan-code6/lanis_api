f447dc6486ba7b45bc37fd665a9362624164fef0
Joan Code <none>
2026-05-01 16:39:59 +0200

    v0.1.10

--- schulportal_hessen/__init__.py
+++ schulportal_hessen/__init__.py
@@ -4,4 +4,4 @@ from schulportal_hessen.base import SchulportalHessenAPI
 from schulportal_hessen.tools.cryptor import Cryptor
 
 __all__ = ["SchulportalHessenAPI", "Cryptor"]
-__version__ = "0.1.0"
+__version__ = "0.1.10"
