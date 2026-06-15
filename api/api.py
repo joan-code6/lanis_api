88ffb5b5d9946472db2e95be70dd190d8820f2f9
Joan Code <bennetjoanwegener@gmail.com>
2026-06-15 15:35:07 +0200

    adding surge ssh

--- api/api.py
+++ api/api.py
@@ -490,7 +490,10 @@ app.add_middleware(
         "http://localhost:5173",
         "https://lanis.arg-server.de",
     ],
-    allow_origin_regex=r"https://.*\.appwrite\.network",
+    allow_origin_regex=[
+        r"https://.*\.appwrite\.network",
+        r"https://.*\.surge.sh",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
